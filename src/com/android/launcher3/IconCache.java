b1d04c066b711dab38146aca61079f7d03896f87
Sunny Goyal <sunnygoyal@google.com>
2014-10-06 16:23:56 -0700

    Updating icons for sortcuts when the target app updates.
    Bug: 17398260
    Change-Id: I055abb971d1f72245e8616ac2ce07bcdf37cdd52

--- src/com/android/launcher3/IconCache.java
+++ src/com/android/launcher3/IconCache.java
@@ -149,7 +149,7 @@ public class IconCache {
         return getFullResIcon(Resources.getSystem(), android.R.mipmap.sym_def_app_icon);
     }
 
-    public Drawable getFullResIcon(Resources resources, int iconId) {
+    private Drawable getFullResIcon(Resources resources, int iconId) {
         Drawable d;
         try {
             d = resources.getDrawableForDensity(iconId, mIconDpi, mContext.getTheme());
